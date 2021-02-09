0c9dfb3248829ea7b07da8667c2bf955270028dd
jacobleesinger <jacobleesinger@gmail.com>
2021-02-08 17:03:13 -0800

    [logger] configure winston logger

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,18 @@ dotenv.config();
 
 interface Config {
   NODE_ENV: string;
+  PAPERTRAIL_PORT: number;
+  LOG_LEVEL: string;
 }
 
 const {
-  NODE_ENV = 'development'
+  NODE_ENV = 'development',
+  PAPERTRAIL_PORT = '',
+  LOG_LEVEL = 'debug'
 } = process.env;
 
 export const config: Config = {
-  NODE_ENV
+  NODE_ENV,
+  PAPERTRAIL_PORT: parseInt(PAPERTRAIL_PORT, 10),
+  LOG_LEVEL
 };
--- src/logger.ts
+++ src/logger.ts
@@ -1,16 +1,33 @@
 import { createLogger, format, transports } from 'winston';
+import { Syslog } from 'winston-syslog';
+import os from 'os';
+
+import { config } from './config';
+
+const localhost = os.hostname();
+const options = {
+  host: 'logs.papertrailapp.com',
+  port: config.PAPERTRAIL_PORT,
+  app_name: 'demo-auth-server',
+  localhost
+};
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
-  // To see more detailed errors, change this to 'debug'
-  level: 'info',
+  // To see more detailed errors, change the LOG_LEVEL env var to debug, i.e. LOG_LEVEL=debug
+  level: config.LOG_LEVEL || 'info', // Turns out Winston defaults to info but just setting explicitly anyway.
   format: format.combine(
     format.splat(),
+    format.timestamp(),
+    format.colorize(),
+    format.errors({ stack: true }),
     format.simple()
   ),
   transports: [
-    new transports.Console()
-  ]
+    new transports.Console(),
+    new Syslog(options)
+  ],
+  silent: config.NODE_ENV === 'test'
 });
 
 export default logger;
