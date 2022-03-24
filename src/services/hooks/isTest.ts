39b57a4544a6ffcd34c691895006fbf744592f00
Ray Smets <rayjsmets@gmail.com>
2022-03-24 08:59:24 -0700

    [Node env] test added when running tests.

--- src/services/hooks/isTest.ts
+++ src/services/hooks/isTest.ts
@@ -2,5 +2,5 @@ import { HookContext } from '@feathersjs/feathers';
 import { config } from '../../config';
 
 export const isTest = (ctx: HookContext): boolean => {
-  return config.NODE_ENV === 'TEST';
+  return config.NODE_ENV === 'test';
 };
