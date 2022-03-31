08744646b86c51f358e09c0a104ac70cc84f30e7
Ray Smets <rayjsmets@gmail.com>
2022-03-31 09:02:23 -0700

    [Declarations] HookContextWithTransaction interface added for new relic metadata usage.

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,7 +1,13 @@
 import { Application as ExpressFeathers } from '@feathersjs/express';
+import { TransactionHandle } from 'newrelic';
+import { HookContext, Service } from '@feathersjs/feathers';
 
 // A mapping of service names to types. Will be extended in service files.
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ServiceTypes {}
 // The application instance type that will be used everywhere else
 export type Application = ExpressFeathers<ServiceTypes>;
+
+export interface HookContextWithTransaction<T = never, S = Service<T>, R = T> extends HookContext {
+    transaction?: TransactionHandle;
+  }
