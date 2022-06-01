961d6e445eff8054be1a85d32608d744cd228d16
Ray Smets <rayjsmets@gmail.com>
2022-05-31 20:30:01 -0700

    [GetAuthPath] service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,11 @@
 import { Application } from '../declarations';
 import AuthService from './auth/auth.service';
+import GetAuthPathService from './getAuthPath/getAuthPath.service';
 import GetAuthUrlService from './getAuthUrl/getAuthUrl.service';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export default function (app: Application): void {
     app.configure(AuthService);
     app.configure(GetAuthUrlService);
+    app.configure(GetAuthPathService);
 }
