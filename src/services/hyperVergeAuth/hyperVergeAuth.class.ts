53866942ce051639ef3daddd262f1f8c4c0319bf
Ray Smets <rayjsmets@gmail.com>
2022-06-07 20:47:44 -0700

    [HyperVergeAuth] service.

---
+++ src/services/hyperVergeAuth/hyperVergeAuth.class.ts
@@ -0,0 +1,41 @@
+import { Params } from '@feathersjs/feathers';
+import { config } from '../../config';
+import { Application } from '../../declarations';
+import logger from '../../logger';
+import { makeFormDataNetworkRequest, makeNetworkRequest, RESTData, RESTResponse } from '../../utils/networkRequestHelper';
+
+interface HyperVergeAuthToken {
+    status: string,
+    statusCode: number,
+    result: {
+        token: string
+    }
+}
+
+export class HyperVergeAuth {
+  app: Application;
+
+  constructor (app: Application) {
+    this.app = app;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async create (data: any, params?: Params): Promise<HyperVergeAuthToken> {
+
+    const restData: RESTData = {
+      method: 'POST',
+      baseUrl: config.HYPER_VERGE_SAAS_URL,
+      endPoint: '/login',
+      header: { },
+      data: {
+        "appId": config.HYPER_VERGE_APP_ID,
+        "appKey": config.HYPER_VERGE_APP_KEY,
+        "expiry": 30000
+      }
+    };
+
+    const response = await makeNetworkRequest<HyperVergeAuthToken>(restData);
+
+    return response.body;
+  }
+}
