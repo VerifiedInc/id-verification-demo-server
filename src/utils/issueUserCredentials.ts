2d9caebd6dccfc0dd17e3274c406fd77e5399b1f
Ray Smets <rayjsmets@gmail.com>
2022-07-19 15:58:12 -0700

    fix

--- src/utils/issueUserCredentials.ts
+++ src/utils/issueUserCredentials.ts
@@ -84,61 +84,69 @@ export async function issueHvUserCredentials (user: UserDto, hvIssuer: IssuerEnt
       type: 'AddressCredential',
       address: user.hvAddress
     });
+  }
 
-    if (user.hvDocImage) {
-      credentialSubjects.push({
-        id: user.did,
-        type: 'GovernmentIdDocumentImageCredential',
-        image: user.hvDocImage
+  if (user.hvDocImage) {
+    credentialSubjects.push({
+      id: user.did,
+      type: 'GovernmentIdDocumentImageCredential',
+      image: user.hvDocImage
     });
+  }
 
-    if (user.hvDocCountry) {
-      credentialSubjects.push({
-        id: user.did,
-        type: 'CountryResidenceCredential',
-        country: user.hvDocCountry
+  if (user.hvDocCountry) {
+    credentialSubjects.push({
+      id: user.did,
+      type: 'CountryResidenceCredential',
+      country: user.hvDocCountry
     });
+  }
 
-    if (user.hvDocType) {
-      credentialSubjects.push({
-        id: user.did,
-        type: 'GovernmentIdTypeCredential',
-        documentType: user.hvDocType
+  if (user.hvDocType) {
+    credentialSubjects.push({
+      id: user.did,
+      type: 'GovernmentIdTypeCredential',
+      documentType: user.hvDocType
     });
+  }
 
-    if (user.hvFaceImage) {
-      credentialSubjects.push({
-        id: user.did,
-        type: 'FacialImageCredential',
-        image: user.hvFaceImage
+  if (user.hvFaceImage) {
+    credentialSubjects.push({
+      id: user.did,
+      type: 'FacialImageCredential',
+      image: user.hvFaceImage
     });
+  }
 
-    if (user.hvLiveFace) {
-      credentialSubjects.push({
-        id: user.did,
-        type: 'LivelinessCredential',
-        liveliness: user.hvLiveFace
+  if (user.hvLiveFace) {
+    credentialSubjects.push({
+      id: user.did,
+      type: 'LivelinessCredential',
+      liveliness: user.hvLiveFace
     });
+  }
 
-    if (user.hvLiveFaceConfidence) {
-      credentialSubjects.push({
-        id: user.did,
-        type: 'LivelinessConfidenceCredential',
-        confidence: user.hvLiveFaceConfidence
+  if (user.hvLiveFaceConfidence) {
+    credentialSubjects.push({
+      id: user.did,
+      type: 'LivelinessConfidenceCredential',
+      confidence: user.hvLiveFaceConfidence
     });
+  }
 
-    if (user.hvFaceMatch) {
-      credentialSubjects.push({
-        id: user.did,
-        type: 'FacialMatchCredential',
-        match: user.hvFaceMatch
+  if (user.hvFaceMatch) {
+    credentialSubjects.push({
+      id: user.did,
+      type: 'FacialMatchCredential',
+      match: user.hvFaceMatch
     });
+  }
 
-    if (user.hvFaceMatchConfidence) {
-      credentialSubjects.push({
-        id: user.did,
-        type: 'FacialMatchConfidenceCredential',
-        facialMatchConfidence: user.hvFaceMatchConfidence
+  if (user.hvFaceMatchConfidence) {
+    credentialSubjects.push({
+      id: user.did,
+      type: 'FacialMatchConfidenceCredential',
+      facialMatchConfidence: user.hvFaceMatchConfidence
     });
   }
 
